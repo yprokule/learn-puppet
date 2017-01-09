7aa09e1f841fbec61f997edaa8b6779743570b40
Yurii Prokulevych <iprokule@gmail.com>
2017-01-09 21:04:10 +0100

    Variable interpolation

--- study-manfifests/notify-resource-variables.pp
+++ study-manfifests/notify-resource-variables.pp
@@ -7,3 +7,9 @@ $msg = 'This message is defined in a variable'
 notify { 'greeting':
     message => $msg,
 }
+
+$username = 'Luke'
+
+notify { "User $username":
+    message =>  "String interpolation. Username is: $username",
+}
