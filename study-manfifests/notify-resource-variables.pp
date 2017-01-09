4d304e3be2aa1e2af94f713ccdf5d12ef60732d2
Yurii Prokulevych <iprokule@gmail.com>
2017-01-09 21:08:26 +0100

    Using array variable

--- study-manfifests/notify-resource-variables.pp
+++ study-manfifests/notify-resource-variables.pp
@@ -13,3 +13,13 @@ $username = 'Luke'
 notify { "User $username":
     message =>  "String interpolation. Username is: $username",
 }
+
+$array = [ 'This', 'is', $username ]
+
+notify { 'list variable':
+    message =>  "This is a list: $array",
+}
+
+notify { 'variable from list':
+    message =>  "'${array[0]}' variable is defined in a list at index [0]",
+}
