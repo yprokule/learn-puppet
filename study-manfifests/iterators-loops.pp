6ddc892288c17617044a3cc0f31682623104e497
Yurii Prokulevych <iprokule@gmail.com>
2017-01-15 18:22:12 +0100

    Iterators-Loops. Iterate over the list

--- study-manfifests/iterators-loops.pp
+++ study-manfifests/iterators-loops.pp
@@ -4,6 +4,12 @@
 
 $array = [ 1, 2, 'three', "${facts[os][family]}", [5, 6] ]
 
-$array.each | $index, $value | {
-    notice("Index: ${index} : Value: ${value}")
+$array.each | $index | {
+    notice("Array. Passing one element to lambda: ${index}")
 }
+
+notice("Array. Passing index/value to lambda")
+$array.each | $index, $value | {
+    notice("Array. Index: ${index} : Value: ${value}")
+}
+
