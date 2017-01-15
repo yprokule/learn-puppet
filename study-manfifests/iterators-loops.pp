8612824388b8f819001afb392d5ec88aef878da0
Yurii Prokulevych <iprokule@gmail.com>
2017-01-15 18:25:48 +0100

    Iterators-Loops. Iterate over DICTIONARY

--- study-manfifests/iterators-loops.pp
+++ study-manfifests/iterators-loops.pp
@@ -1,7 +1,7 @@
 #
 # Iterators and Loops
 #
-
+warning("Iterations over the list")
 $array = [ 1, 2, 'three', "${facts[os][family]}", [5, 6] ]
 
 $array.each | $index | {
@@ -13,3 +13,18 @@ $array.each | $index, $value | {
     notice("Array. Index: ${index} : Value: ${value}")
 }
 
+warning("Iteration over the dict")
+$dict = {
+    'first name'    =>  'John',
+    'last name'     =>  'Tribianney',
+    'title'         =>  'Mr',
+    'occupation'    =>  'Actor',
+}
+
+$dict.each | $element | {
+    notice("Dict. Iteration over: ${element}")
+}
+
+$dict.each | $key, $value | {
+    notice("Dict. Key: '${key}'   => Value: '${value}'")
+}
