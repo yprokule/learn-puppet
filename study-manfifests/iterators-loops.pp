2ddcbbbfbcbeeb0bdf167e44ef1d729bbcfe24df
Yurii Prokulevych <iprokule@gmail.com>
2017-01-15 18:29:34 +0100

    Iterators-Loops. Iterate over dict with one parameter passed to  the lambda.

--- study-manfifests/iterators-loops.pp
+++ study-manfifests/iterators-loops.pp
@@ -5,10 +5,10 @@ warning("Iterations over the list")
 $array = [ 1, 2, 'three', "${facts[os][family]}", [5, 6] ]
 
 $array.each | $index | {
-    notice("Array. Passing one element to lambda: ${index}")
+    notice("Array. Passing one paramater to lambda: ${index}")
 }
 
-notice("Array. Passing index/value to lambda")
+notice("Array. Passing index/value parameters to lambda")
 $array.each | $index, $value | {
     notice("Array. Index: ${index} : Value: ${value}")
 }
@@ -25,6 +25,12 @@ $dict.each | $element | {
     notice("Dict. Iteration over: ${element}")
 }
 
+notice("Passing key/value pairs to lambda")
 $dict.each | $key, $value | {
     notice("Dict. Key: '${key}'   => Value: '${value}'")
+}
+
+notice("Passing one parameter to lamda")
+$dict.each | $element | {
+    notice("Dict.OneParamater. Element[0]: '${element[0]}'   => Element[1]: '${element[1]}'")
 }
