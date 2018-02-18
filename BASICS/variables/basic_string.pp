c1dc83710e44aa55351937dfd962a4e965578ac0
Yurii Prokulevych <iprokule@gmail.com>
2018-02-18 14:09:08 +0100

    [Single quoted strings] Example

--- BASICS/variables/basic_string.pp
+++ BASICS/variables/basic_string.pp
@@ -5,3 +5,14 @@
 notify { hello_world:
     message =>  this_is_bare_string,
 }
+
+$msg = 'This is defined variable'
+
+notify { 'single_quote_string_1':
+    message =>  'This is single quoted message
+                 Line breaks are treated as line breaks :)',
+}
+
+notify { 'single_quote_string_2':
+    message =>  'Variable ${msg} is defined but not interpolated',
+}
