87eefea2221db6222adb65c10acf8daced38e168
Yurii Prokulevych <iprokule@gmail.com>
2018-02-18 14:44:36 +0100

    [HEREDOC] Example

--- BASICS/variables/basic_string.pp
+++ BASICS/variables/basic_string.pp
@@ -20,3 +20,37 @@ notify { 'single_quote_string_2':
 notify { "double_quoted_string_1":
     message =>  "Variable \$msg has value '${msg}'",
 }
+
+$availability_zone = 'AZ:1:0:15'
+$auth_user = 'AZ_User_1'
+$auth_pass = 'e59ff97941044f85df5297e1c302d260'
+
+$format_text = @("FMTXT"/Lt)
+  [section1]
+    # This is line1
+    # This is line 2
+    # Interpolation is working with heredoc
+    availability_zone = ${availability_zone}
+    range\t=\t35:55
+
+  [section2]
+    # This is line1 section-2
+    # This is line1 section-2
+    # Interpolation is working with heredoc
+    auth_user       = ${auth_user}
+    auth_password   = ${auth_pass}
+
+  |-FMTXT
+
+notify { 'heredoc':
+    message =>  "${format_text}",
+}
+
+$heredoc_file = '/tmp/here_doc_example.txt'
+
+file { $heredoc_file:
+    mode    =>  '0755',
+    owner   =>  'nobody',
+    group   =>  'nobody',
+    content =>  $format_text,
+}
